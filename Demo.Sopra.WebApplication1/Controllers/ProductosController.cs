8b08bbe10d33926e60301361cf8333d27fffba2d
Francisco de Borja Cabeza Rozas <borjacabeza@msn.com>
2022-04-19 15:04:21 +0200

    Commit 19-04-2022

--- Demo.Sopra.WebApplication1/Controllers/ProductosController.cs
+++ Demo.Sopra.WebApplication1/Controllers/ProductosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Demo.Sopra.WebApplication1.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
@@ -24,12 +25,30 @@ namespace Demo.Sopra.WebApplication1.Controllers
 
         public IActionResult Ficha(int id)
         {
+            ViewData["Categorias"] = new SelectList(_context.Categories.ToList(), "CategoryID", "CategoryName");
+            ViewData["Proveedores"] = new SelectList(_context.Suppliers.ToList(), "SupplierID", "CompanyName");
+
             var producto = _context.Products
                 .Include(r => r.Category)
                 .Where(r => r.ProductID == id)
                 .FirstOrDefault();
 
             return View(producto);
+        }
+
+        [HttpPost]
+        public IActionResult Ficha(int id, Product product)
+        {
+            if(id != product.ProductID) return NotFound();
+
+            if(ModelState.IsValid)
+            {
+                _context.Update(product);
+                _context.SaveChanges();
+
+                return RedirectToAction("index");
+            }
+            else return View(product);
         }
 
         public ProductosController(ModelNorthwind context)
--- Demo.Sopra.WebApplication1/Models/Product.cs
+++ Demo.Sopra.WebApplication1/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,15 +14,37 @@ namespace Demo.Sopra.WebApplication1.Models
             Order_Details = new HashSet<Order_Detail>();
         }
 
+        [Display(Name = "Referencia")]
+        [Required(ErrorMessage = "La <b>Referencía</b> es un dato requerido.")]        
         public int ProductID { get; set; }
+
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "La <b>Descripción</b> es un dato requerido.")]
+        [MinLength(5, ErrorMessage = "Debe contener al menos 5 caracteres.")]        
         public string ProductName { get; set; }
+
+        [Display(Name = "Proveedor")]
         public int? SupplierID { get; set; }
+
+        [Display(Name = "Categoría")]
         public int? CategoryID { get; set; }
+
+        [Display(Name = "Cantidades por unidad")]
         public string QuantityPerUnit { get; set; }
+
+        [Display(Name = "Precio")]
         public decimal? UnitPrice { get; set; }
+
+        [Display(Name = "Stock")]
         public short? UnitsInStock { get; set; }
+
+        [Display(Name = "Stock Pedido")]
         public short? UnitsOnOrder { get; set; }
+
+        [Display(Name = "Nivel")]
         public short? ReorderLevel { get; set; }
+
+        [Display(Name = "Descuentos")]
         public bool Discontinued { get; set; }
 
         public virtual Category Category { get; set; }
